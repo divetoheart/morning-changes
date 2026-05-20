024d33cf2d3cb7eec03f2acb6799487b6eda3c74
divetoheart <95659693+divetoheart@users.noreply.github.com>
2026-05-20 12:47:25 -0700

    Add chord library module

---
+++ chord-data.js
@@ -0,0 +1,9 @@
+const chords = {
+  Cm7: { fn: 'ii in Bb', shell: ['Eb','Bb'], arp: ['C','Eb','G','Bb'] },
+  F7: { fn: 'V in Bb', shell: ['A','Eb'], arp: ['F','A','C','Eb'] },
+  Bbmaj7: { fn: 'I in Bb', shell: ['D','A'], arp: ['Bb','D','F','A'] },
+  Ebmaj7: { fn: 'IV in Bb', shell: ['G','D'], arp: ['Eb','G','Bb','D'] },
+  Am7b5: { fn: 'iiø in Gm', shell: ['C','G'], arp: ['A','C','Eb','G'] },
+  D7: { fn: 'V in Gm', shell: ['F#','C'], arp: ['D','F#','A','C'] },
+  Gm7: { fn: 'i in Gm', shell: ['Bb','F'], arp: ['G','Bb','D','F'] }
+};
