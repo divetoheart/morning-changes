bdc301a6d144f1a9a1fe366a34f8d20d1d9d238d
divetoheart <95659693+divetoheart@users.noreply.github.com>
2026-06-30 21:41:00 -0700

    Load application themes

--- apps/web/src/entry.tsx
+++ apps/web/src/entry.tsx
@@ -1,16 +1,22 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { MusicTypography } from './MusicTypography';
+import { ThemeDock } from './ThemeDock';
+import { ThemeProvider } from './ThemeProvider';
 import './styles.css';
 import './lesson-rendering.css';
 import './notation.css';
+import './themes.css';
 
 const mountNode = document.querySelector('#root');
 
 if (mountNode) {
   createRoot(mountNode).render(
-    <MusicTypography>
-      <App />
-    </MusicTypography>
+    <ThemeProvider>
+      <MusicTypography>
+        <App />
+        <ThemeDock />
+      </MusicTypography>
+    </ThemeProvider>
   );
 }
