531261d2cc53208a7b938c8e164fbc0c98996688
divetoheart <95659693+divetoheart@users.noreply.github.com>
2026-07-01 10:45:45 -0700

    Route native After Hours standards to their own pages

--- apps/web/src/AfterHoursAutumnLeavesApp.tsx
+++ apps/web/src/AfterHoursAutumnLeavesApp.tsx
@@ -1,5 +1,8 @@
-import { AfterHoursPortPreview } from './AfterHoursPortPreview';
+import { useLocation } from 'react-router-dom';
+import { AfterHoursAutumnPort } from './AfterHoursAutumnPort';
+import { AfterHoursBluesApp } from './AfterHoursBluesApp';
 
 export function AfterHoursAutumnLeavesApp() {
-  return <AfterHoursPortPreview />;
+  const location = useLocation();
+  return location.pathname.includes('/12-bar-blues') ? <AfterHoursBluesApp /> : <AfterHoursAutumnPort />;
 }
