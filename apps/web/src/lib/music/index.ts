87f7cfbfe1b88e11aa9c1635ae661194d8e3d661
divetoheart <95659693+divetoheart@users.noreply.github.com>
2026-07-01 16:01:32 -0700

    Expose canonical music engine public API

---
+++ apps/web/src/lib/music/index.ts
@@ -0,0 +1,7 @@
+export * from './types';
+export * from './intervals';
+export * from './pitch';
+export * from './harmony';
+export * from './fretboard';
+export * from './shapes';
+export * from './notation';
